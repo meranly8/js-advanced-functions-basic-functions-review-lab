fe9fb388c4c8318ef2624822a075804f00dbffdb
Michelle Ranly <meranly8@gmail.com>
2021-04-08 22:02:19 -0500

    commit tree 6165e66140472777d3b58db344f25185ddbea417

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(char="*") {
+    return function(adj="special"){
+        return `You are ${char}${adj}${char}!`
+    }
+}
+
+const Calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => {
+        return a - b
+    },
+    multiply: (a, b) => {
+        return a * b
+    },
+    divide: (a, b) => {
+        return a / b
+    }
+
+}
+
+function actionApplyer(start, ftnArry) {
+   ftnArry.forEach(ftn => {
+       start = ftn(start)
+   })
+   return start
+}
