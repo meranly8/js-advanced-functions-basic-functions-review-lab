80b24c344c999c74a3d5e848c4f73a729dd8d501
Michelle Ranly <meranly8@gmail.com>
2021-04-08 22:08:13 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(char="*") {
+    return function(adj="special"){
+        return `You are ${char}${adj}${char}!`
+    }
+}
+
+const Calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+function actionApplyer(start, ftnArry) {
+   ftnArry.forEach(ftn => {
+       start = ftn(start)
+   })
+   return start
+}
